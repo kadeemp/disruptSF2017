fb7ccb4aa8a4da15f21de9ebd6f2a6f387b9c0e5
nkhaja <nbl.khaja@gmail.com>
2017-09-16 14:46:22 -0700

    Initial Commit

---
+++ BgLive/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BgLive
+//
+//  Created by Nabil K on 2017-09-16.
+//  Copyright © 2017 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
