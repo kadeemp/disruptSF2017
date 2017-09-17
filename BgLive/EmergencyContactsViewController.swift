e39bd1981da7c2f463ca420a4def7fcb66515039
nkhaja <nbl.khaja@gmail.com>
2017-09-16 18:29:55 -0700

    starting work on emergency contact controller

---
+++ BgLive/EmergencyContactsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EmergencyContactsViewController.swift
+//  BgLive
+//
+//  Created by Nabil K on 2017-09-16.
+//  Copyright © 2017 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class EmergencyContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
