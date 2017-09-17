e809b6fd13edfec922f745eff0fc81a74f827d17
nkhaja <nbl.khaja@gmail.com>
2017-09-16 18:52:24 -0700

    updating Emergency Contact cell

---
+++ BgLive/EContactCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  EContactCollectionViewCell.swift
+//  BgLive
+//
+//  Created by Nabil K on 2017-09-16.
+//  Copyright © 2017 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class EContactCollectionViewCell: UICollectionViewCell {
+    
+    // Name
+    // Picture
+    
+    
+    
+}
--- BgLive/EmergencyContactsViewController.swift
+++ BgLive/EmergencyContactsViewController.swift
@@ -9,27 +9,39 @@
 import UIKit
 
 class EmergencyContactsViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
+    
+    var allContacts = [Contact]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+}
+
+
+extension EmergencyContactsViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allContacts.count
     }
-    */
-
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let contact = self.allContacts[IndexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "e-cell", for: indexPath) as! EContactCollectionViewCell
+        
+        
+        return cell
+    }
+    
 }
